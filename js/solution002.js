c17bdfdc1f54426e9fd530c30a2fb67eb2e218b9
Stephen Kim <steph.dev.k@Stephens-MBP.attlocal.net>
2022-05-20 00:58:01 -0700

    solution002

---
+++ js/solution002.js
@@ -0,0 +1,15 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+    //your code here
+    //P boolean
+    //R as string
+    //E true => 'true', false = 'false'
+    //P 
+    //toString method
+    return b.toString()
+  }
+
+console.log(booleanToString(true))
