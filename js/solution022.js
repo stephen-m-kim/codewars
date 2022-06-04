1a0f6692bf96a8fb2573f29b548d85272b910433
Stephen Kim <steph.dev.k@Stephens-MBP.attlocal.net>
2022-06-04 00:00:43 -0700

    solution022

---
+++ js/solution022.js
@@ -0,0 +1,19 @@
+// he code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+// Task
+// Fix the bug so we can all go home early.
+
+// Notes
+// String str will never be null.
+
+
+var replaceDots = function(str) {
+    //take a str with dots '.'
+    //replace '.' with '-'
+    //split dot join dash
+    return str.split('.').join('-')
+}
+
+console.log(replaceDots('one.two.three.four.ten'))
